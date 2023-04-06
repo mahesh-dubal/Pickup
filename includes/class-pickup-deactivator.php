8bf3d2b15e5b1fd67985c87b97292a7cc23c2e72
mahesh4320 <mahesh.dubal@wisdmlabs.com>
2023-04-06 13:43:23 +0530

    folders added using plugin boilerplate

---
+++ includes/class-pickup-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://mahesh-d.wisdmlabs.net
+ * @since      1.0.0
+ *
+ * @package    Pickup
+ * @subpackage Pickup/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Pickup
+ * @subpackage Pickup/includes
+ * @author     Mahesh Dubal <mahesh.dubal@wisdmlabs.com>
+ */
+class Pickup_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
