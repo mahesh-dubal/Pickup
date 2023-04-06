806a883d9dc4349cd423d619926d4fa0cfc1304d
mahesh4320 <mahesh.dubal@wisdmlabs.com>
2023-04-06 13:57:40 +0530

    hook added to create a custom post type

--- includes/class-pickup.php
+++ includes/class-pickup.php
@@ -157,6 +157,10 @@ class Pickup {
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 
+		//To create a custom post type
+		$this->loader->add_action('init', $plugin_admin, 'create_store_post_type');
+
+
 	}
 
 	/**
